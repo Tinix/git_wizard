a6f2acc433b4107f66f8d75fdeb60a89cbfbe61c
Daniel Tinivella <dtinivella@gmail.com>
2024-10-15 12:37:55 -0300

    add new changes to project

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() {
         println!("5. Push current branch");
         println!("6. Merge current branch into develop");
         println!("7. Merge current branch into main");
-        println!("8. Exit");
+        println!("8. Show git status");
+        println!("9. Exit");
 
         print!("Choose an option: ");
         io::stdout().flush().unwrap(); // Limpia la línea para que el prompt aparezca correctamente
@@ -26,15 +27,19 @@ fn main() {
         match option {
             "1" => list_branches(),
             "2" => create_branch(),
-            "3" => {
+            "3" => add_changes(),
+            "4" => commit_changes(),
+            "5" => {
                 if let Some(branch_name) = get_current_branch() {
                     push_branch(&branch_name);
                 }
             }
-            "4" => merge_branch("develop"),
-            "5" => {
+            "6" => merge_branch("develop"),
+            "7" => merge_branch("main"),
+            "8" => show_git_status(),
+            "9" => {
                 println!("Exiting Git Wizard...");
-                break; // Salir del bucle
+                break;
             }
             _ => println!("Invalid option. Please try again."),
         }
@@ -71,6 +76,36 @@ fn create_branch() {
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
 
+fn add_changes() {
+    let add = Command::new("git")
+        .arg("add")
+        .output()
+        .expect("Failed to add changes");
+
+    println!("{}", String::from_utf8_lossy(&add.stdout));
+}
+
+fn commit_changes() {
+    print!("Enter the commit message: ");
+    io::stdout().flush().unwrap();
+
+    let mut commit_message = String::new();
+    io::stdin()
+        .read_line(&mut commit_message)
+        .expect("Failed to read line");
+
+    let commit_message = commit_message.trim();
+
+    let commit = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(commit_message)
+        .output()
+        .expect("Failed to commit changes");
+
+    println!("{}", String::from_utf8_lossy(&commit.stdout));
+}
+
 fn push_branch(branch_name: &str) {
     let push = Command::new("git")
         .arg("push")
@@ -91,6 +126,22 @@ fn merge_branch(target_branch: &str) {
         .expect("Failed to merge branch");
 
     println!("{}", String::from_utf8_lossy(&merge.stdout));
+}
+
+fn show_git_status() {
+    let status = Command::new("git")
+        .arg("status")
+        .output()
+        .expect("Failed to show git status");
+
+    const RED: &str = "\u{001b}[31m";
+    const RESET: &str = "\u{001b}[0m";
+    println!(
+        "{}{}{}",
+        RED,
+        String::from_utf8_lossy(&status.stdout),
+        RESET
+    );
 }
 
 fn get_current_branch() -> Option<String> {
